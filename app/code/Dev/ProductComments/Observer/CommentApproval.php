87371cdbe25adcb09c6c01bbfee342d28429f714
nikashio <nikashio42@gmail.com>
2019-09-02 18:05:04 +0400

    Email sending with magento built in functions

--- app/code/Dev/ProductComments/Observer/CommentApproval.php
+++ app/code/Dev/ProductComments/Observer/CommentApproval.php
@@ -2,39 +2,55 @@
 
 namespace Dev\ProductComments\Observer;
 
-use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Store\Model\Store;
 use Dev\ProductComments\Block\View;
+use Exception;
 
 class CommentApproval implements ObserverInterface
 {
+    protected $transportBuilder;
     /**
      * @var View
      */
     private $view;
 
     public function __construct(
-     View $view
+        TransportBuilder $transportBuilder,
+        View $view
     ) {
-
+        $this->transportBuilder = $transportBuilder;
         $this->view = $view;
     }
-
-    public function execute(\Magento\Framework\Event\Observer $observer)
+    public function execute(Observer $observer)
     {
-        $productId = $observer->getData('ProductId');
-        $product = $this->view->getProductName($productId);
+        $email = $observer->getData('sentEmail');
         $comment = $observer->getData('sentComment');
         $name = $observer->getData('SenderName');
-        $to = $observer->getData('sentEmail');
-        $subject = 'Your Comment is waiting for admin approval.';
-        $message = 'Hellow '.$name.' your comment is currently beeing reviewed.' . "\r\n" .
-            'you left comment for: '.$product . "\r\n" .
-            'your comment is: '.$comment;
-        $headers = 'From: magento@shopify.com' . "\r\n" .
-            'Reply-To: Dev-alliance@xacho.com' . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
-        mail($to,$subject,$message,$headers);
+        $productId = $observer->getData('ProductId');
+        $product = $this->view->getProductName($productId);
+        $sender = [
+            'name' => 'Magento team',
+            'email' => 'Dev-alliance@magento.com',
+        ];
+        $templateParams = [
+            'comment' => $comment,
+            'name' => $name,
+            'product' =>$product
+        ];
+        $transport = $this->transportBuilder
+            ->setTemplateIdentifier('commentvisibility_email_template')
+            ->setTemplateOptions(['area' => 'frontend', 'store' => Store::DEFAULT_STORE_ID])
+            ->addTo($email)
+            ->setTemplateVars($templateParams)
+            ->setFrom($sender)
+            ->getTransport();
+        try {
+            $transport->sendMessage();
+        } catch (Exception $e) {
+        }
+        return $this;
     }
 }
-
